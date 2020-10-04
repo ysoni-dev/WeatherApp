84831cb979216aecd92cc7cec5d917ad142be521
ysoni-dev <ysoni1614@gmail.com>
2020-10-04 15:21:49 +0530

    Updates

---
+++ precache-manifest.cf9490ed57035045e675075a9d2b147c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87ac45fe7ab1be1b9434adef626eaebe",
+    "url": "/Weatherapp/index.html"
+  },
+  {
+    "revision": "b704d8f40536b46094c2",
+    "url": "/Weatherapp/static/css/main.5331024b.chunk.css"
+  },
+  {
+    "revision": "54bdea4185d9be84f069",
+    "url": "/Weatherapp/static/js/2.0506fbc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weatherapp/static/js/2.0506fbc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b704d8f40536b46094c2",
+    "url": "/Weatherapp/static/js/main.8899f5e3.chunk.js"
+  },
+  {
+    "revision": "9eaf5e590b0d39a99b4b",
+    "url": "/Weatherapp/static/js/runtime-main.df354785.js"
+  },
+  {
+    "revision": "1000f77336f07b78dd2b87fb31aedfd8",
+    "url": "/Weatherapp/static/media/morning-mob.1000f773.jpg"
+  },
+  {
+    "revision": "72f0ee3e9cbc5b0b10648eefa395e249",
+    "url": "/Weatherapp/static/media/morning-pc.72f0ee3e.jpg"
+  },
+  {
+    "revision": "418f10aef8ba6fe33fb89484511bb788",
+    "url": "/Weatherapp/static/media/night-mob.418f10ae.png"
+  },
+  {
+    "revision": "40c4ca3cda55ab2bd1b2ed01e9db6f83",
+    "url": "/Weatherapp/static/media/nightpc.40c4ca3c.jpg"
+  }
+]);
